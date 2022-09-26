1eabe517f478752b6465f1e0ebc3542bb3dab44e
Yury Getman <imblowfish@duck.com>
2022-09-27 01:50:07 +0400

    implements base controls ui

--- ui/controls.tsx
+++ ui/controls.tsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Draggable from "react-draggable";
-import { Fab } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import { Avatar, ButtonGroup, IconButton } from "@mui/material";
+import { Stack } from "@mui/system";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import CameraRoundedIcon from "@mui/icons-material/CameraRounded";
+import PlayCircleFilledRoundedIcon from "@mui/icons-material/PlayCircleFilledRounded";
+import PauseCircleFilledRoundedIcon from "@mui/icons-material/PauseCircleFilledRounded";
+import StopCircleRoundedIcon from "@mui/icons-material/StopCircleRounded";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 
 import { Id } from "../islands/enums";
 
 // NOTE @imblowfish: https://www.npmjs.com/package/react-draggable
 const Controls = () => {
+  const [isPlay, setIsPlay] = useState(false);
+
   return (
     <Draggable>
-      <Fab
+      <Stack
         sx={{
           position: "fixed",
           bottom: "100px",
+          zIndex: 2147483647,
         }}
-        color='primary'
-        aria-label='add'
+        direction='column'
+        alignItems='center'
+        justifyContent='center'
+        spacing={1}
       >
-        <AddIcon />
-      </Fab>
+        <IconButton>
+          <CloseRoundedIcon fontSize='large' />
+        </IconButton>
+        <Avatar sx={{ width: 200, height: 200 }}>
+          <CameraRoundedIcon fontSize='large' />
+        </Avatar>
+        <ButtonGroup>
+          <IconButton disabled={!isPlay}>
+            <DeleteRoundedIcon fontSize='large' />
+          </IconButton>
+          <IconButton onClick={() => setIsPlay((value) => !value)}>
+            {isPlay ? (
+              <PauseCircleFilledRoundedIcon fontSize='large' />
+            ) : (
+              <PlayCircleFilledRoundedIcon fontSize='large' />
+            )}
+          </IconButton>
+          <IconButton disabled={!isPlay}>
+            <StopCircleRoundedIcon fontSize='large' />
+          </IconButton>
+        </ButtonGroup>
+      </Stack>
     </Draggable>
   );
 };
