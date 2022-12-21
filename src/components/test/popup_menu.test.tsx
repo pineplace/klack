2afc825f9573faa7f686d3b8c10091de372f89de
Yury Getman <imblowfish@duck.com>
2022-12-22 02:19:07 +0400

    fixes `popup_menu` tests

--- src/components/test/popup_menu.test.tsx
+++ src/components/test/popup_menu.test.tsx
@@ -3,7 +3,13 @@
  */
 // NOTE: https://testing-library.com/docs/
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import { builder, sender } from "../../messaging";
 import PopupMenu from "../popup_menu";
 
@@ -16,44 +22,68 @@ jest.mock("../../messaging", () => {
       stopRecording: jest.fn(),
       showCameraBubble: jest.fn(),
       hideCameraBubble: jest.fn(),
+      getter: {
+        recordingInProgress: jest.fn(),
+      },
     },
     sender: {
-      send: jest.fn().mockResolvedValue({}),
+      send: jest.fn().mockResolvedValue({ result: false }),
     },
   };
 });
 
-test("ShowHideRecording switching", () => {
-  render(<PopupMenu />);
+test("ShowHideRecording switching", async () => {
+  await act(() => {
+    render(<PopupMenu />);
+  });
 
   const button = screen.getByText("Show bubble");
 
-  fireEvent.click(button);
+  await act(() => {
+    fireEvent.click(button);
+  });
 
   expect(builder.showCameraBubble).toHaveBeenCalled();
   expect(sender.send).toHaveBeenCalled();
   expect(button.textContent).toEqual("Hide bubble");
 
-  fireEvent.click(button);
+  await act(() => {
+    fireEvent.click(button);
+  });
 
   expect(builder.hideCameraBubble).toHaveBeenCalled();
   expect(sender.send).toHaveBeenCalled();
   expect(button.textContent).toEqual("Show bubble");
 });
 
-test("StartStopRecording switching", () => {
-  render(<PopupMenu />);
+test("StartStopRecording switching", async () => {
+  const { rerender } = await act(() => {
+    return render(<PopupMenu />);
+  });
 
   const button = screen.getByText("Start");
 
-  fireEvent.click(button);
+  await act(() => {
+    (sender.send as jest.Mock).mockResolvedValue({ result: true });
+    fireEvent.click(button);
+    rerender(<PopupMenu />);
+  });
 
   expect(builder.startRecording).toHaveBeenCalled();
   expect(sender.send).toHaveBeenCalled();
-  expect(button.textContent).toEqual("Stop");
+  await waitFor(() => {
+    expect(button.textContent).toEqual("Stop");
+  });
 
-  fireEvent.click(button);
+  await act(() => {
+    (sender.send as jest.Mock).mockResolvedValue({ result: false });
+    fireEvent.click(button);
+    rerender(<PopupMenu />);
+  });
+
   expect(builder.stopRecording).toHaveBeenCalled();
   expect(sender.send).toHaveBeenCalled();
-  expect(button.textContent).toEqual("Start");
+  await waitFor(() => {
+    expect(button.textContent).toEqual("Start");
+  });
 });
