d909e561465de2e1745821ceb968430b60bb7962
Yury Getman <imblowfish@duck.com>
2023-04-06 22:41:28 +0400

    Fixed `@emotion/react` warning on multiple camera bubble component injection

--- src/handlers.ts
+++ src/handlers.ts
@@ -91,6 +91,24 @@ export async function handleHideCameraBubble(_args: MethodArgs): Promise<void> {
     target: { tabId: await storage.get.cameraBubbleTabId() },
     func: () => {
       document.getElementById("rapidrec-camera-bubble")?.remove();
+      /* NOTE: `@emotion/react` library used by `@mui` as a style library sets the value in
+       *       `globalThis` to define double import. This is ok in case of normal use of `@mui`
+       *       in web UI.
+       *       But when show/hide the camera bubble via Chrome code injection, we expect
+       *       the component using `@mui` to be injected and removed multiple times.
+       *       Setting the value to `globalThis` in such a situation results in warning,
+       *       which is displayed to the user in the `Errors` tab for extension.
+       *       For this purpose, such a hack was invented
+       *
+       *       More info: https://github.com/imblowfish/rapidrec-chrome-extension/issues/118
+       */
+      const re = new RegExp(/__EMOTION_REACT_\d+__/);
+      const emotionKey = Object.keys(globalThis).find((key) => re.test(key));
+      if (!emotionKey) {
+        return;
+      }
+      // @ts-expect-error `globalThis` uses `any`, we're trying to set the key to `string`, TS doesn't like that
+      globalThis[emotionKey] = false;
     },
   });
   await storage.set.cameraBubbleTabId(0);
