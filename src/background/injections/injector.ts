60d89314fbf1fb6638dd9c4dff999f8e3eb87e02
Yury Getman <imblowfish@duck.com>
2025-02-11 01:55:17 +0400

    Added `Injector` class

---
+++ src/background/injections/injector.ts
@@ -0,0 +1,36 @@
+export enum Injection {
+  CameraBubble = "./camera_bubble.bundle.mjs",
+}
+
+export enum InjectionElementId {
+  CameraBubble = "klack-camera-bubble",
+}
+
+export class Injector {
+  static inject(tabId: number, injection: Injection) {
+    console.log(
+      `[injector.ts] Injector::inject(tabId=${tabId}, injection=${injection})`,
+    );
+    return chrome.scripting.executeScript({
+      target: {
+        tabId,
+      },
+      files: [injection],
+    });
+  }
+
+  static async deinject(tabId: number, elementId: InjectionElementId) {
+    console.log(
+      `[injector.ts] Injector::deinject(tabId=${tabId}, elementId=${elementId})`,
+    );
+    return chrome.scripting.executeScript({
+      target: {
+        tabId,
+      },
+      args: [elementId],
+      func: (elementId: InjectionElementId) => {
+        document.getElementById(elementId)?.remove();
+      },
+    });
+  }
+}
