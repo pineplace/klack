add3c1cee85563a67e954543a998237d0e311eaa
Yury Getman <imblowfish@duck.com>
2025-05-06 22:17:46 +0400

    Removed recording start countdown component because it was the last component which used MUI

--- src/app/injections.ts
+++ src/app/injections.ts
@@ -1,9 +1,7 @@
 export enum Injection {
   CameraBubble = "./camera_bubble.bundle.mjs",
-  RecordingStartCounter = "./recording_start_countdown.bundle.mjs",
 }
 
 export enum InjectionElementId {
   CameraBubble = "klack-camera-bubble",
-  RecordingStartCounter = "klack-recording-start-counter",
 }
