9c78e9b368db05a1886eb3192c118e658f2d2126
Yury Getman <imblowfish@duck.com>
2025-05-19 23:30:08 +0400

    Removed unused `forwardRef` and moved `link` element creation from `camera_bubble.ts` to `CameraBubble.tsx`

--- src/ui/pages/popup/components/RecButton.tsx
+++ src/ui/pages/popup/components/RecButton.tsx
@@ -1,17 +1,15 @@
-import React, { forwardRef } from "react";
+import React from "react";
 
-export const RecButton = forwardRef<
-  HTMLButtonElement,
-  Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "className">
->((props, ref) => {
+export const RecButton = (
+  props: Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "className">,
+) => {
   return (
     <button
-      ref={ref}
       className="bg-klack-red-500 hover:bg-klack-red-600 active:bg-klack-red-500 flex size-[64px] items-center justify-center rounded-full"
       {...props}
     >
       <div className="relative size-[52px] rounded-[100%] border-4 border-white"></div>
     </button>
   );
-});
+};
 RecButton.displayName = "RecButton";
